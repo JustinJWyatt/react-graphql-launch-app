08198869ac5f9e88c2172e2dd03557219b723f09
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-17 09:56:19 -0600

    added launch route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import ApolloClient  from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 import logo from './logo.jpg';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
@@ -18,7 +19,8 @@ class App extends Component {
           <div className="container">
             <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }}/>
             <Route exact path="/" component={Launches}/>
-          
+            <Route exact path="/launch:flight_number" component={Launch}/>
+
           </div>
         </Router>
       </ApolloProvider>
---
+++ client/src/components/Launch.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Launch extends Component {
+  render(){
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Launch;
