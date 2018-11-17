fb5ae6211709e1f70ccb7022b2a01722ad4c1bdd
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-17 16:44:09 -0600

    added details

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -56,6 +56,29 @@ class Launch extends Component {
                 <h1 className="display-4 my-3">
                   <span className="text-dark">Mission: </span> {mission_name}
                 </h1>
+
+                <h4 className="mb-3">Launch Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Flight Number: {flight_number}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Year: {launch_year}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Successful: {' '} <span className={classNames({
+                      'text-success': launch_success,
+                      'text-danger': !launch_success
+                    })}>{launch_success ? 'Yes' : 'No'}</span>
+                  </li>
+                </ul>
+
+                <h4 className="my-3">Rocket Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">Rocket ID: {rocket_id}</li>
+                  <li className="list-group-item">Rocket Name: {rocket_name}</li>
+                  <li className="list-group-item">Rocket Type: {rocket_type}</li>
+                </ul>
               </div>
             );
           }}
