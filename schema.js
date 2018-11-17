26fa14e924a21c1b26d61dc5eda00deac9f32895
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-17 00:20:56 -0600

    added object types

--- schema.js
+++ schema.js
@@ -1,4 +1,6 @@
-const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean } = require('graphql');
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLSchema, GraphQLList } = require('graphql');
+
+const axios = require('axios');
 
 // Launch GraphQLObjectType
 const LaunchType = new GraphQLObjectType({
@@ -9,6 +11,32 @@ const LaunchType = new GraphQLObjectType({
     launch_year: { type: GraphQLString },
     launch_date_local: { type: GraphQLString },
     launch_success: { type: GraphQLBoolean },
-    rocket: { type: RocketType },
+    rocket: { type: RocketType }
   })
-})
+});
+
+const RocketType = new GraphQLObjectType({
+  name: 'Rocket',
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args){
+          return axios.get('https://api.spacexdata.com/v3/launches')
+                      .then(res => res.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
