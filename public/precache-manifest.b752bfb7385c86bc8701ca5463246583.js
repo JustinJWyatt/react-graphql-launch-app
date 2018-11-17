286711a1decda7818ee92eb7ec622d7fb5d48366
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-17 16:54:36 -0600

    added public build

---
+++ public/precache-manifest.b752bfb7385c86bc8701ca5463246583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dab9996f15513443ab7ec4d1c04cab9",
+    "url": "/static/media/logo.7dab9996.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7a4b6d5879d0437f2a0b",
+    "url": "/static/js/main.7a4b6d58.chunk.js"
+  },
+  {
+    "revision": "25c4c260ff81c349a298",
+    "url": "/static/js/1.25c4c260.chunk.js"
+  },
+  {
+    "revision": "7a4b6d5879d0437f2a0b",
+    "url": "/static/css/main.2d5b56f2.chunk.css"
+  },
+  {
+    "revision": "efb4373b893cbac31586a690c45ef3a1",
+    "url": "/index.html"
+  }
+];
